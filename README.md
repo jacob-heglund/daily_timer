7df66986f140d5e1a6ca856a2e3c219234eb60e2
Jacob Heglund <jheglun2@gmail.com>
2022-05-19 13:05:04 -0500

    finalized timer

---
+++ README.md
@@ -0,0 +1,21 @@
+# Work Timer
+
+Based on 90-minute work periods followed by 20-minute rest periods. This work schedule was suggested by Dr. Andrew Huberman for higher levels of focus during work periods, as well as improved recovery during break periods.
+
+Developed and tested on Windows. Some of the notifications and sounds are likely to break on other operating systems.
+
+## Generating an .exe file
+
+Based on [this video](https://www.youtube.com/watch?v=UZX5kH72Yx4).
+
+To generate `main.exe` run the following in `daily_timer/`:
+
+```bash
+pyinstaller --onefile main.py
+mv dist/main.exe .
+rm -rf build
+rm -rf dist
+rm main.spec
+```
+
+Then double click on `main.exe` to start the timer.
--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+import os
 import argparse
 import time
 import datetime
-from plyer import notification
 from playsound import playsound
+import plyer.platforms.win.notification
+from plyer import notification
+import webbrowser
 import pdb
 
 parser = argparse.ArgumentParser(description=None)
-parser.add_argument('-n', '--n_sessions', default=2, type=int, help="Number of consecutive work sessions you want to plan")
+parser.add_argument('-n', '--n_sessions', default=2, type=int, help="Number of consecutive work sessions you want to plan. Only works for n \in {1, 2} since you're probably not able to do more than two consecutive work sessions.")
 parser.add_argument('-w', '--work_length', default=90, type=int, help="Number of minutes per work session")
 parser.add_argument('-r', '--rest_length', default=20, type=int, help="Number of minutes per rest session")
+parser.add_argument('-d', '--debug_mode', default=False, type=str, choices=('True', 'False'), help="Debug mode")
+# parser.add_argument('-d', '--debug_mode', default=True, type=str, choices=('True', 'False'), help="Debug mode")
 args = parser.parse_args()
+
+nsdr_url = 'https://youtu.be/pL02HRFk2vo?t=48/'
+playlist_url = 'https://www.youtube.com/playlist?list=PLylfBmdQ1h3ZjMnIAA7_wTv9Rh8hR1r9W'
+
+
+def open_nsdr_url():
+    webbrowser.open_new(nsdr_url)
+
+
+def open_playlist_url():
+    webbrowser.open_new(playlist_url)
 
 
 class Period:
-    def __init__(self, period_type, start_time, duration):
+    def __init__(self, period_type, start_time, duration, time_index=0):
         self.period_type = period_type
         self.start_time = start_time
         self.duration = duration
         self.end_time = self.start_time + datetime.timedelta(minutes=duration)
+        self.time_index = time_index
 
 
 class WorkTimer:
@@ -25,11 +42,20 @@ class WorkTimer:
         self.n_sessions = args.n_sessions
         self.work_length = args.work_length
         self.rest_length = args.rest_length
+        self.debug_mode = args.debug_mode
         self.lunch = True
+        self.time_format = "%I:%M:%p"
+        self.start_work_soundfile = "./resources/message-ringtone-magic.mp3"
+        self.start_rest_soundfile = "./resources/fingerlicking-message-tone.mp3"
+        self.notification_length = 1
 
-        #TODO bring back in real time after development is done
-        self.start_time = datetime.datetime(2022, 5, 17, 8, 30)
-        # self.start_time = datetime.datetime.now()
+        self.app_name = "Work Timer"
+        self.app_icon_path = "./resources/clock.ico"
+
+        if self.debug_mode:
+            self.start_time = datetime.datetime(2022, 5, 17, 8, 51)
+        else:
+            self.start_time = datetime.datetime.now()
 
         # Cut off any work sessions after the first one so I can have lunch between 11:00 - 11:59 AM
         latest_lunch_start = self.start_time.replace(hour=11, minute=59)
@@ -40,115 +66,143 @@ class WorkTimer:
         self.times = [self.start_time]
 
         self._create_plan()
+
         end_times = []
         for session in self.sessions.values():
             for period in session.values():
                 end_times.append(period.end_time)
         self.max_end_time = max(end_times)
-        self._print_plan()
+        self.plan_str = self._print_plan()
+
         self._run_plan()
 
     def _create_plan(self):
         for i in range(self.n_sessions):
             self.sessions[i] = {}
-            if i < 1:
-                self._add_work_session(self.sessions[i])
+            if i == 0:
+                if i == self.n_sessions - 1:
+                    self._add_work_session(self.sessions[i], final_session=True)
+                else:
+                    self._add_work_session(self.sessions[i])
             else:
                 if self.lunch:
                     self._add_lunch_session(self.sessions[i], self.sessions[i-1])
                 else:
                     self._add_work_session(self.sessions[i], self.sessions[i-1])
 
+
     def _print_plan(self):
         # show the entire plan for the work sessions, prints to the command line
-        time_format = "%I:%M:%p"
+        plan_str = ""
 
         for key in self.sessions:
             session = self.sessions[key]
             print_str = ""
             for i in session:
-                print_str += session[i].period_type.capitalize() + f" {session[i].start_time.strftime(time_format)} - {session[i].end_time.strftime(time_format)}"
-                if session[i].period_type == "work":
-                    print_str += "\n"
-            print(print_str)
+                print_str += session[i].period_type.capitalize() + f" {session[i].start_time.strftime(self.time_format)} - {session[i].end_time.strftime(self.time_format)}"
+                print_str += "\n"
+            plan_str += print_str
+
+        print(plan_str)
+        return plan_str
 
     def _run_plan(self):
-        #TODO remove after development is done
-        curr_time = datetime.datetime(2022, 5, 17, 8, 30)
+        if self.debug_mode:
+            curr_time = datetime.datetime(2022, 5, 17, 10, 00)
+
         done = False
         prev_period = None
+        last_print_len = None
 
         while not done:
-            #TODO add after development is done
             # every minute get the current time and check which session we're in
-            # curr_time = datetime.datetime.now()
-
-            #TODO remove after development is done
-            curr_time += datetime.timedelta(minutes=1)
-            if curr_time > self.max_end_time:
-                done = True
-                break
+            if self.debug_mode:
+                curr_time += datetime.timedelta(minutes=1)
+            else:
+                curr_time = datetime.datetime.now()
 
             # run a timer for each session in the commandline
             for session in self.sessions.values():
                 for period in session.values():
                     if period.start_time <= curr_time < period.end_time:
                         curr_period = period
-                        remaining_time = (curr_time - period.end_time).total_seconds() / 60
-                        print(f"{period.period_type.capitalize()} --- Remaining time: {abs(round(remaining_time, 0))}", end="\r")
+                        remaining_time_minutes = int(abs((curr_time - period.end_time).total_seconds() / 60))
 
-            #TODO have some behavior that tells you when your work session is over and to start resting
-            ## maybe a notification of some type?
-            #TODO I couldn't get it to work as an exe file on Windows using pyinstaller. It kept not seeing the libraries I needed for it to compile
+                        ## printing option 1: updates only the timer
+                        # if last_print_len is not None:
+                        #     print(" "*last_print_len, end="\r")
+                        # msg = f"{period.period_type.capitalize()} --- Remaining time (minutes): {remaining_time_minutes}"
+                        # print(msg, end="\r")
+                        # last_print_len = len(msg)
+
+                        ## printing option 2: updates the timer and has a little pointer to the current period
+                        os.system("cls")
+                        tmp_str = self.plan_str.split("\n")
+                        tmp_str[curr_period.time_index] += " <--- You are here"
+                        for i in range(len(tmp_str)-1):
+                            tmp_str[i] += "\n"
+                        new_plan_str = ''.join(tmp_str)
+                        print(new_plan_str)
+
+                        msg = f"{period.period_type.capitalize()} --- Remaining time (minutes): {remaining_time_minutes}"
+                        print(msg)
+
+            # push notification on start of focused work
             if prev_period is None:
                 prev_period = curr_period
+                playsound(self.start_work_soundfile, block=False)
+                title = f"Focused work until {curr_period.end_time.strftime(self.time_format)}"
+                msg = f"{self.work_length} minutes of focused work."
+                notification.notify(title, msg, app_name=self.app_name, app_icon=self.app_icon_path)
 
             if curr_period != prev_period:
                 if curr_period.period_type == "work":
-                    print("\n")
-                    print("Work started")
-                    print("\n")
-                    notification.notify(title = "Work Started", timeout = 10)
+                    playsound(self.start_work_soundfile, block=False)
+                    title = f"Focused work until {curr_period.end_time.strftime(self.time_format)}"
+                    msg = f"{self.work_length} minutes of focused work."
+                    notification.notify(title, msg, app_name=self.app_name, app_icon=self.app_icon_path)
+
                 elif curr_period.period_type == "rest":
-                    print("\n")
-                    print("Work ended")
-                    print("\n")
-                    playsound("resources/tatls_listen.mp3")
-                    notification.notify(title = "Work Ended", timeout = 10)
+                    playsound(self.start_rest_soundfile, block=False)
+                    title = f"Rest until {curr_period.end_time.strftime(self.time_format)}"
+                    msg = f"{self.rest_length} minute break, try going on a walk or do some NSDR."
+                    notification.notify(title, msg, app_name=self.app_name, app_icon=self.app_icon_path)
 
             prev_period = curr_period
+            if curr_time > self.max_end_time:
+                if not self.debug_mode:
+                    playsound(self.start_rest_soundfile, block=False)
+                    notifier.show_toast("Work session ended", msg=f"Go have some lunch or go on a walk or something.", duration=self.notification_length)
+                done = True
+                break
 
-            # have a sound play when the session ends
+            if self.debug_mode:
+                time.sleep(0.1)
+            else:
+                time.sleep(60)
 
-            ## do behaviors based on the current session
-            ## TODO add white noise sound as an option during work sesions
-
-            #TODO add after development is done
-            time.sleep(0.1)
-            # time.sleep(60)
-
-    def _add_work_session(self, session, prev_session=None):
-        # work session ends 90 minutes after start of work session first
+    def _add_work_session(self, session, prev_session=None, final_session=False):
+        # work session ends self.work_length minutes after start of work session first
         if prev_session is None:
             session["work"] = Period("work", self.start_time, self.work_length)
         else:
-            session["work"] = Period("work", prev_session["rest"].end_time, self.work_length)
+            session["work"] = Period("work", prev_session["rest"].end_time, self.work_length, time_index=prev_session["rest"].time_index + 1)
 
-        session["rest"] = Period("rest", session["work"].end_time, self.rest_length)
+        if not final_session:
+            session["rest"] = Period("rest", session["work"].end_time, self.rest_length, time_index=session["work"].time_index + 1)
 
     def _add_lunch_session(self, session, prev_session):
         # cut down the work time for this work session to make it end at 11:59 AM for lunch
         work_length = self.work_length
-        work_end = prev_session["work"].start_time + datetime.timedelta(minutes=self.work_length)
+        work_end = prev_session["rest"].end_time + datetime.timedelta(minutes=self.work_length)
         latest_lunch_start = self.start_time.replace(hour=11, minute=59)
         if work_end > latest_lunch_start:
             work_diff_minutes = (latest_lunch_start - work_end).total_seconds() / 60
             work_length = self.work_length - abs(work_diff_minutes)
 
-        session["work"] = Period("work", prev_session["rest"].end_time, work_length)
+        session["work"] = Period("work", prev_session["rest"].end_time, work_length, time_index=prev_session["rest"].time_index + 1)
 
 
-#TODO turn into .exe so I can run it in a windows commandline
 if __name__ == "__main__":
-    timer = WorkTimer(args)
-    k = input("Press any key to exit")
+    WorkTimer(args)
+
